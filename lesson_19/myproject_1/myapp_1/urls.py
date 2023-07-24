0c5dd997fb0976b42467a7d2661cd4b7385c969a
Валерия <valeria2606@list.ru>
2023-07-24 04:26:57 +0300

    Create new file

---
+++ lesson_19/myproject_1/myapp_1/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import PostListView, PostCreateView,  PostUpdateView, PostDeleteView
+
+
+urlpatterns = [
+    path('posts/', PostListView.as_view(), name='post_list'),
+    path('posts/create/', PostCreateView.as_view(), name='post_create'),
+    path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+
+]
+
